b29ab94815822d8e4175081670c217d483159960
Homray <st094140@student.spbu.ru>
2023-05-17 22:41:22 +0300

    Главная адаптирована под телефон

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-var boss = document.getElementById("boss")
-
-boss.onclick = function(){
-    boss.style.color = "red";
-}
-
 $(document).ready(function() {
     $('.header_burger').click(function(event) {
         $('.header_burger, .menu').toggleClass('active');
